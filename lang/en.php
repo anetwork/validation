579eb0a8c8bcf28489d87dfe23932fa33d0bccdc
Shahrokh Niakan <sh.niakan@anetwork.ir>
2016-09-21 16:23:20 +0330

    Added some changes on lang files

--- lang/en.php
+++ lang/en.php
@@ -2,19 +2,20 @@
 
 return [
 
-	'persian_num'		=> 'The :attribute must be a persian number.',
-	'persian_alpha_num'	=> 'The :attribute must be a persian alpahbet or number.',
-	'iran_mobile'		=> 'The :attribute must be a iran mobile number.',
-	'sheba'				=> 'The :attribute must be a sheba number.',
-	'melli_code'		=> 'The :attribute must be a iran melli code.',
-	'is_not_persian'	=> 'The :attribute could not be contain persian alpahbet or number.',
-	'limited_array'		=> 'The :attribute must ba a array and contain values you define not more.',
-	'unsigned_num'		=> 'The :attribute must be an integer and unsigned.',
-	'alpha_space'		=> 'The :attribute must be alphabet.',
-	'a_url'				=> 'The :attribute is not correct url.',
-	'a_domain'			=> 'The :attribute is not correct domain.',
-	'more'				=> 'The :attribute must be more than parameter.',
-	'less'				=> 'The :attribute must be less than parameter.',
-	'iran_phone'		=> 'The :attribute must be a iran phone number.'
+	'persian_alpha' 	=> 'must be a persian alpahbet',
+	'persian_num'		=> 'must be a persian number',
+	'persian_alpha_num'	=> 'must be a persian alpahbet or number',
+	'iran_mobile'		=> 'must be a iran mobile number',
+	'sheba'				=> 'must be a sheba number',
+	'melli_code'		=> 'must be a iran melli code',
+	'is_not_persian'	=> 'could not be contain persian alpahbet or number',
+	'limited_array'		=> 'must ba a array and contain values you define not more',
+	'unsigned_num'		=> 'must be an integer and unsigned',
+	'alpha_space'		=> 'must be alphabet and space',
+	'a_url'				=> 'url is not correct',
+	'a_domain'			=> 'domain is not correct domain',
+	'more'				=> 'must be more than parameter',
+	'less'				=> 'must be less than parameter',
+	'iran_phone'		=> 'must be a iran phone number'
 
 ];
