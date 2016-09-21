4f0a6d580905f9b8780facf2206ac26bd2f0d1a0
Shahrokh Niakan <sh.niakan@anetwork.ir>
2016-09-21 15:16:15 +0330

    Added en and fa lang file for validation error

--- src/ValidationMessages.php
+++ src/ValidationMessages.php
@@ -7,248 +7,236 @@ namespace Anetwork\Validation;
  */
 class ValidationMessages
 {
-	/**
-     * @var string
-	 */
-	 protected $msg;
+
+	 /**
+	   * @var string
+	  */
+	  protected $lang;
+
+	  /**
+		* @var array
+	   */
+	   protected $config;
+
+	  /**
+	   * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
+	   * @since Sep 21, 2016
+	   */
+	 public function __construct() {
+
+		 $this->lang = \App::getLocale();
+
+		 $this->config = include_once __DIR__ . '/../lang/' . $this->lang . '.php';
+
+	 }
 
 	 /**
 	  * validation message for Alpha
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function AlphaMsg($message, $attribute)
+	 public function AlphaMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a persian alphabet.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for Num
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function NumMsg($message, $attribute)
+	 public function NumMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a persian number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for AlphaNum
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function AlphaNumMsg($message, $attribute)
+	 public function AlphaNumMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a persian alpahbet or number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for IranMobile
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function IranMobileMsg($message, $attribute)
+	 public function IranMobileMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a iran mobile number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for Sheba
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function ShebaMsg($message, $attribute)
+	 public function ShebaMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a sheba number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for MelliCode
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function MelliCodeMsg($message, $attribute)
+	 public function MelliCodeMsg($message, $attribute,	$rule)
 	 {
 
-		 $this->msg = "The $attribute must be a iran melli code.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for IsNotPersian
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function IsNotPersianMsg($message, $attribute)
+	 public function IsNotPersianMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute could not be contain persian alpahbet or number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for LimitedArray
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function LimitedArrayMsg($message, $attribute)
+	 public function LimitedArrayMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must ba a array and contain values you define not more.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for UnSignedNum
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function UnSignedNumMsg($message, $attribute)
+	 public function UnSignedNumMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be an integer and unsigned.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for AlphaSpace
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function AlphaSpaceMsg($message, $attribute)
+	 public function AlphaSpaceMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be alphabet.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for Url
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function UrlMsg($message, $attribute)
+	 public function UrlMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute is not correct url.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for Domain
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function DomainMsg($message, $attribute)
+	 public function DomainMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute is not correct domain.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for More
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function MoreMsg($message, $attribute)
+	 public function MoreMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be more than parameter.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for Less
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function LessMsg($message, $attribute)
+	 public function LessMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be less than parameter.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
 	 /**
 	  * validation message for IranPhone
-	  * @param $message, $attribute
+	  * @param $message, $attribute and $rule
 	  * @author Shahrokh Niakan <sh.niakan@anetwork.ir>
 	  * @since Sep 11, 2016
 	  * @return string
 	  */
-	 public function IranPhoneMsg($message, $attribute)
+	 public function IranPhoneMsg($message, $attribute, $rule)
 	 {
 
-		 $this->msg = "The $attribute must be a iran phone number.";
-
-		 return str_replace($message, $this->msg, $message);
+		 return str_replace($message, $this->config[ $rule ], $message);
 
 	 }
 
